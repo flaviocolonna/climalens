77eca4cf111e9ad08efafc0fcfaa40c217444112
Flavio Colonna <colonnaromanoflavio@gmail.com>
2026-08-27 12:05:27 +0200

    Update import!

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { LocaleProvider } from '@/i18n/LocaleProvider';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
