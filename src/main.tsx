64938ed70c702be32754bfff24bdf2cd3c3e64c1
Flavio Colonna <colonnaromanoflavio@gmail.com>
2026-08-27 12:02:10 +0200

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { LocaleProvider } from '@/i18n/LocaleProvider';
+import { Analytics } from "@vercel/analytics/next"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LocaleProvider>
+      <Analytics />
       <App />
     </LocaleProvider>
   </StrictMode>,
